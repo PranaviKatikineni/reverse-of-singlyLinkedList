15b9b12c1eb83f739fbcf3e912ddc138176979fa
PranaviKatikineni <ksaipranavi@gmail.com>
2024-12-11 15:23:46 +0530

    Add files via upload

---
+++ SinglyLinkedList (3).java
@@ -0,0 +1,71 @@
+import java.util.*;
+class Node
+{
+	int data;
+	Node next;
+	Node(int num)
+	{
+		this.data=num;
+		this.next=null;
+	}
+}
+class SinglyLinkedList
+{
+	Node head = null;
+	Node temp = null;
+	Node prev=null;
+	public void insertNode(int num)
+	{
+		Node newNode = new Node(num);
+		if (head == null)
+		{
+			head = temp = newNode;
+		}
+		else
+		{
+			temp.next = newNode;
+			temp = newNode;
+		}
+	}
+	public void reversePrint(Node node)
+	{
+	    if(node==null)
+	    {
+	        return;
+	    }
+	    reversePrint(node.next);
+	    System.out.println(node.data);
+	}
+	public void display()
+	{
+		temp=head;
+		while(temp.next!=null)
+		{
+			System.out.print(temp.data+" ");
+			temp=temp.next;
+		}
+		System.out.print(temp.data+"\n");
+	}
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		int t =s.nextInt();
+		for(int i=1; i<=t; i++)
+		{
+			SinglyLinkedList list = new SinglyLinkedList();
+			int num;
+			System.out.println("Enter the list"+" "+i);
+			while (true)
+			{
+				num = s.nextInt();
+				if (num == -1)
+				{
+					break;
+				}
+				list.insertNode(num);
+			}
+		//	System.out.println("Reversed list: ");
+            list.reversePrint(list.head);
+		}
+	}
+}
